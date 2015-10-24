laowantong/mocodo

commit 20dfdd4b90fcfb0870bb0489d425d38b342d3539

fix: under Jupiter Notebook, %mocodo fails until the option --input is **not** provided
By default, the magic command puts its stuff in a sub-directory
`mocodo_notebook` of the current working directory. The option
`--input` prevented its creation when it did not exist.

nb_magic/mocodo.py
@@ -57,11 +57,6 @@ class MocodoMagics(Magics):
         
         input_path = notebook_options.input
         if not input_path:
-            try: 
-                os.makedirs("mocodo_notebook")
-            except OSError:
-                if not os.path.isdir("mocodo_notebook"):
-                    raise
             input_path = "mocodo_notebook/sandbox.mcd"
             codecs.open(input_path, "w", "utf8").write(cell)
         elif not os.path.isfile(input_path) and os.path.isfile(input_path + ".mcd"):
@@ -70,6 +65,11 @@ class MocodoMagics(Magics):
         output_dir = notebook_options.output_dir
         if not output_dir:
             output_dir = "mocodo_notebook"
+        try:
+            os.makedirs(output_dir)
+        except OSError:
+            if not os.path.isdir(output_dir):
+                raise
         output_name = os.path.join(output_dir, os.path.splitext(os.path.split(input_path)[1])[0])
         
         options.extend(["--input", input_path, "--output_dir", output_dir, "--image_format", "svg"]) # may override user's provided options

mocodo/version_number.py
@@ -1,1 +1,1 @@
-version = u"2.0.9"
+version = u"2.0.10"

commit a0556b594e98f661746affb8fa40349f0c2c469e

fix: %mocodo still fails to create its output dir
Fix a regression bug introduced by the commit
20dfdd4b90fcfb0870bb0489d425d38b342d3539

nb_magic/mocodo.py
@@ -55,6 +55,11 @@ class MocodoMagics(Magics):
         
         input_path = notebook_options.input
         if not input_path:
+            try:
+                os.makedirs("mocodo_notebook")
+            except OSError:
+                if not os.path.isdir("mocodo_notebook"):
+                    raise
             input_path = "mocodo_notebook/sandbox.mcd"
             codecs.open(input_path, "w", "utf8").write(cell)
         elif not os.path.isfile(input_path) and os.path.isfile(input_path + ".mcd"):
@@ -63,11 +68,12 @@ class MocodoMagics(Magics):
         output_dir = notebook_options.output_dir
         if not output_dir:
             output_dir = "mocodo_notebook"
-        try:
-            os.makedirs(output_dir)
-        except OSError:
-            if not os.path.isdir(output_dir):
-                raise
+        else:
+            try:
+                os.makedirs(output_dir)
+            except OSError:
+                if not os.path.isdir(output_dir):
+                    raise
         output_name = os.path.join(output_dir, os.path.splitext(os.path.split(input_path)[1])[0])
         
         options.extend(["--input", input_path, "--output_dir", output_dir, "--image_format", "svg"]) # may override user's provided options