01e71e384e5fb833e22eff1cd5e5f0f78a56d9ea
laowantong <aristide@macmini-2.local>
2016-06-29 09:39:59 +0200

    typo

--- mocodo/cross.py
+++ mocodo/cross.py
@@ -17,7 +17,7 @@ crossed_strings = frozenset(["-++-","-++0","-+0-","-0+-","0++-","+--+","0--+","+
 @memoize
 def cross((x1, y1, x2, y2, x3, y3, x4, y4)):
     """ Tests whether the segments ((x1,y1), (x2,y2)) and ((x3,y3), (x4,y4)) intersect.
-        Two segments sharing exactly one extremity are NOT considered as interesecting. """
+        Two segments sharing exactly one extremity are NOT considered as intersecting. """
     a = (x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)
     b = (x4-x3)*(y2-y3) - (y4-y3)*(x2-x3)
     c = (x2-x1)*(y3-y1) - (y2-y1)*(x3-x1)
