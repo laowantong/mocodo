e5032a9b225cdc0e39c73fcc580ad58a6f37665c
laowantong <aristide@mac-mini-de-aristide-2-1.home>
2016-08-12 19:07:04 +0200

    make setup.py work in Python 3

--- nb_magic/mocodo.py
+++ nb_magic/mocodo.py
@@ -2,9 +2,9 @@
 # encoding: utf-8
 
 from __future__ import division
-
 from __future__ import absolute_import
 from __future__ import print_function
+
 from IPython.core.display import HTML
 from IPython.core.display import SVG
 from IPython.core.display import display
--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ from setuptools import setup
 from codecs import open
 from os import path
 from mocodo.version_number import version
-from io import open
 
 long_description = """
 Mocodo is an open-source tool for designing and teaching relational databases. It takes as an input a textual description of both entities and associations of an entity-relationship diagram (ERD). It outputs a vectorial drawing in SVG and a relational schema in various formats (SQL, LaTeX, Markdown, etc.).
