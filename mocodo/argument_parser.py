a16325224360a82a39501ccb12b1d4bb685c4207
laowantong <aristide@mac-mini-de-aristide-2-1.home>
2016-08-12 16:03:10 +0200

    modernize unicode type

--- mocodo/argument_parser.py
+++ mocodo/argument_parser.py
@@ -15,6 +15,7 @@ import gettext
 import locale
 from time import time
 from io import open
+import six
 
 DESCRIPTION = """
 NAME:
@@ -171,9 +172,9 @@ def parsed_arguments():
     mocodo_group.add_argument("--version", action="version", version="%(prog)s " + version, help="display the version number, then exit")
     mocodo_group.add_argument("--restore", action="store_true", help="recreate a pristine version of the files 'sandbox.mcd' and 'params.json' in the input directory, then exit")
     
-    aspect_group.add_argument("--df", metavar="STR", type=unicode, default=u"DF", help="the acronym to be circled in a functional dependency")
-    aspect_group.add_argument("--card_format", metavar="STR", type=unicode, nargs="?", default=u"{min_card},{max_card}", help="format string for minimal and maximal cardinalities")
-    aspect_group.add_argument("--strengthen_card", metavar="STR", type=unicode, nargs="?", default=u"_1,1_", help="string for relative cardinalities")
+    aspect_group.add_argument("--df", metavar="STR", type=six.text_type, default=u"DF", help="the acronym to be circled in a functional dependency")
+    aspect_group.add_argument("--card_format", metavar="STR", type=six.text_type, nargs="?", default=u"{min_card},{max_card}", help="format string for minimal and maximal cardinalities")
+    aspect_group.add_argument("--strengthen_card", metavar="STR", type=six.text_type, nargs="?", default=u"_1,1_", help="string for relative cardinalities")
     source_group.add_argument("--flex", metavar="FLOAT", type=float, default=0.75, help="flex straight legs whose cardinalities may collide")
     aspect_group.add_argument("--tkinter", action="store_true", help="use Tkinter to calculate the pixel-dimensions of the labels")
     aspect_group.add_argument("--colors", metavar="PATH", default="bw", help="the color palette to use when generating the drawing. Name (without extension) of a file located in the directory 'colors', or path to a personal file")
