9d2632236746416a8ef0df220e531d92afb9bfbd
laowantong <aristide.grange@univ-lorraine.fr>
2022-07-01 12:19:12 +0200

    Update test_grid.py

--- test/test_grid.py
+++ test/test_grid.py
@@ -8,15 +8,15 @@ class GridTests(unittest.TestCase):
     
     def test_constructor(self):
         grid = Grid(16)
-        assert grid == [None, (1, 1), (2, 2), (2, 2), (2, 2), (3, 2), (3, 2), (3, 3), (3, 3), (3, 3), (4, 3), (4, 3), (4, 3), (5, 3), (5, 3), (5, 3), (4, 4)]
+        assert grid == [None, (1, 1), (2, 1), (3, 1), (2, 2), (3, 2), (3, 2), (3, 3), (3, 3), (3, 3), (4, 3), (4, 3), (4, 3), (5, 3), (5, 3), (5, 3), (4, 4)]
     
     def test_nth_next(self):
         grid = Grid(16)
-        assert grid[3] == (2, 2)
-        assert grid.get_nth_next(3, 0) == (2, 2)
-        assert grid.get_nth_next(3, 1) == (3, 2)
-        assert grid.get_nth_next(3, 2) == (3, 3)
-        assert grid.get_nth_next(3, 3) == (4, 3)
+        assert grid[3] == (3, 1)
+        assert grid.get_nth_next(3, 0) == (3, 1)
+        assert grid.get_nth_next(3, 1) == (2, 2)
+        assert grid.get_nth_next(3, 2) == (3, 2)
+        assert grid.get_nth_next(3, 3) == (3, 3)
 
 if __name__ == '__main__':
     unittest.main()
