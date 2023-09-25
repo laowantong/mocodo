80093a0f3663950c9ae5e5b8bc2bf050a198650d
laowantong <laowantong@users.noreply.github.com>
2023-09-25 12:29:04 +0200

    add MSSQL (Microsoft SQL Server) support

--- mocodo/dev/build_template_index.py
+++ mocodo/dev/build_template_index.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 from collections import defaultdict
 import json
+import os
 
 from mocodo.tools import load_mini_yaml
 
-template_folder_path = Path(f"mocodo/resources/relation_templates")
+# Build the index of templates
+
+folder = Path(f"mocodo/resources/relation_templates")
 aliases = defaultdict(list)
 metadata = defaultdict(dict)
-for path in sorted(template_folder_path.glob("*.yaml")):
+for path in sorted(folder.glob("*.yaml")):
     if "-" in path.name:
         continue
     data = load_mini_yaml.run(path)
@@ -25,4 +28,21 @@ result = json.dumps(metadata, indent=2, ensure_ascii=False)
 result = result.replace("[\n      ", "[")
 result = result.replace("\n    ]", "]")
 result = result.replace(",\n      ", ", ")
-Path(template_folder_path, "_index.json").write_text(result + "\n")
+Path(folder, "_index.json").write_text(result + "\n")
+
+# Build the graph of templates
+
+edges = []
+for path in sorted(folder.glob("*.yaml")):
+    data = load_mini_yaml.run(path)
+    color = min(9, len(data))
+    font_color = "white" if color > 5 else "black"
+    if "parent" in data:
+        edges.append(f'"{data["parent"]}" -> "{path.stem}"')
+    edges.append(f'"{path.stem}" [fillcolor={color} fontcolor={font_color}]')
+edges = "\n  ".join(edges)
+result = f'digraph G {{\n  rankdir=LR\n  node [shape=box style="rounded,filled" colorscheme=reds9 fontname="Arial" penwidth=0]\n  {edges}\n}}\n'
+path = Path(folder, "_graph.gv")
+path.write_text(result)
+os.system(f"dot -Tsvg {folder}/_graph.gv > {folder}/_graph.svg")
+path.unlink()
