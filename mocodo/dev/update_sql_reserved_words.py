0e0558b3256c6ff6d72f1587cd6501b128065785
laowantong <laowantong@users.noreply.github.com>
2023-10-11 22:14:05 +0200

    protect reserved keywords in each SQL dialect
    Scrape https://modern-sql.com/reserved-words-empirical-list

---
+++ mocodo/dev/update_sql_reserved_words.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import re
+import requests
+
+url = "https://modern-sql.com/reserved-words-empirical-list.html"
+text = requests.get(url).text
+
+words = {}
+for (y, word) in re.findall(r'y="(\d+)"><tspan class="code.*?">(.+?)</tspan>', text):
+    words[str(int(y) - 14)] = word
+
+words_by_dialect = {}
+dialect = None
+for chunk in text.split('<g class="hoverable">'):
+    if m := re.search(r'rotate\(-45\)">(.+?)</text>', chunk):
+        dialect = m[1]
+        words_by_dialect[dialect] = []
+        for (href, y) in re.findall(r'<use href="#(.+?)" transform="translate\(\d+,(\d+)\)"/>', chunk):
+            if href != "none":
+                words_by_dialect[dialect].append(words[y])
+
+dialects = {
+    "Apache Derby": "",
+    "BigQuery": "",
+    "Db2 (LUW)": "",
+    "H2": "",
+    "MariaDB": "",
+    "MySQL": "mysql.yaml",
+    "Oracle DB": "oracle.yaml",
+    "PostgreSQL": "postgresql.yaml",
+    "SQL Server": "mssql.yaml",
+    "SQLite": "sqlite.yaml",
+}
+
+for (dialect, filename) in dialects.items():
+    if not filename:
+        continue
+    alternative = "|".join(words_by_dialect[dialect])
+    path = Path("mocodo", "resources", "relation_templates", filename)
+    text = path.read_text()
+    text = re.sub(r"('Protect reserved keywords'\n    search: ).+", fr"\1'(?i)^({alternative})$'", text)
+    path.write_text(text)
+    print(f"{filename} updated.")
