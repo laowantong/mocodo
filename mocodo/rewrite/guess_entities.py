e4de69a33fdafba63d3cf36da63500c276c4acfe
laowantong <laowantong@users.noreply.github.com>
2023-10-22 08:38:12 +0200

    suppress date "id." prefix when guessing entities

--- mocodo/rewrite/guess_entities.py
+++ mocodo/rewrite/guess_entities.py
@@ -7,5 +7,11 @@ def run(source, id_prefix):
     entity_name_refs =set(node.children[0].children[0].value for node in tree.find_data("entity_name_ref"))
     entity_name_defs = set(node.children[0].children[0].value for node in tree.find_data("entity_name_def"))
     new_entity_names = entity_name_refs - entity_name_defs
-    new_entity_clauses = '\n'.join(f"{ent}: {id_prefix}{ent.lower()}, " for ent in new_entity_names)
-    return f"{source}\n{new_entity_clauses}"
+    new_entity_clauses = [source]
+    for ent in new_entity_names:
+        if ent.lower() != "date":
+            clause = f"{ent}: {id_prefix}{ent.lower()}, "
+        else:
+            clause = f"{ent}: date"
+        new_entity_clauses.append(clause)
+    return "\n".join(new_entity_clauses)
